e9981bb0a295d4ab802e3ce7f57e0213c7a40143
Yecats77 <liusitong327@gmail.com>
2024-03-24 20:35:54 +0800

    [2024/3/24] DB Details linechart

--- src/pages/database/crustdb_main/detail.vue
+++ src/pages/database/crustdb_main/detail.vue
@@ -80,90 +80,26 @@
             </el-descriptions>
         </div>
 
-        <!-- Gene Annotation -->
-        <!-- <div class="w-330 mt-15 ml-10">
-            <div class="flex flex-row w-350 border-b-2 border-gray-300">
-                <div class="text-4xl font-500 mb-8">Gene Annotation</div>
-            </div>
-            <div class="flex flex-row w-350 justify-between mb-5 mt-8">
-                <el-button @click="downloadtsv">
-                    <template #icon>
-                        <n-icon>
-                            <di />
-                        </n-icon>
-                    </template>
-                    Download Gene Annotation Data
-                </el-button>
-            </div>
-            <div class="w-350" v-loading="loaddata">
-                <n-data-table
-                    :data="proteinList"
-                    :columns="columns"
-                    :pagination="pagination"
-                    :max-height="250"
-                    :row-key="rowKey"
-                />
-            </div>
-        </div> -->
-
-        <!-- Annotated Genome Map -->
         <div class="mt-5 ml-15">
             <div class="flex flex-row w-200">
-                <div class="text-2xl font-500 mb-5">Annotated Genome Map</div>
-                <div class="mt-1.5 ml-10">
-                    <el-button @click="showswicth" v-show="annoshow">View in Circo</el-button>
-                    <el-button @click="showswicth" v-show="!annoshow">View in Detail</el-button>
+                <h1 class="text-3xl mt-9 ml-7 font-500 text-[#3262a8]">Line Plot</h1>
+            </div>
+            <!-- <div class="flex flex-row justify-between mt-6 ml-8 w-285">
+                <n-form-item label="Please choose plot type" class="w-100">
+                    <n-select
+                        v-model:value="linechartvalue"
+                        :options="linechartoptions"
+                        @update:value="detailsdata"
+                    ></n-select>
+                </n-form-item>
+            </div> -->
+            <div class="flex flex-row">
+                <div class="w-300 h-150 mb-10 mt-5 p-5 ml-8" style="box-shadow: 0 0 64px #cfd5db">
+                    <div id="myEcharts" class="h-140" ref="echartlineDom"></div>
                 </div>
             </div>
-            <div
-                style="box-shadow: 0 0 64px #cfd5db"
-                class="w-310 h-200 mt-5 ml-10 mb-20"
-                v-show="annoshow"
-            >
-                <!-- <annotation /> -->
-                <!-- <details_linechart /> -->
-                <img
-                    src="'https://crustdbapi.deepomics.org/files/Axolotls/20DPI_1.VLMC_3DXO/20DPI_1.VLMC_3DXO_details_linechart.png'"
-                />
-            </div>
-        </div>
-        <div
-            style="box-shadow: 0 0 64px #cfd5db"
-            class="w-350 h-250 ml-2 mt-5 mb-20"
-            v-show="!annoshow"
-        >
-            <!-- <circo /> -->
-            <!-- <details_linechart /> -->
-            <img
-                src="../../../../../crustdb_api/workspace/crustdb_database/Axolotls/2DPI_1.MSN_2NN3/2DPI_1.MSN_2NN3_details_linechart.png"
-            />
-        </div>
-        <div class="mt-20 ml-10" v-show="phageStore.phageterminator.length !== 0">
-            <terminator />
-        </div>
-        <div class="mt-20 ml-10" v-show="phageStore.phagetrna.length !== 0">
-            <trna />
-        </div>
-        <div class="mt-20 ml-10" v-show="phageStore.phageanti.length !== 0">
-            <anti />
-        </div>
-        <div class="mt-20 ml-10" v-show="phageStore.phagecrispr.length !== 0">
-            <crispr />
-        </div>
-        <div
-            class="mt-20 ml-10"
-            v-show="phageStore.phagearvf.ar.length !== 0 || phageStore.phagearvf.vf.length !== 0"
-        >
-            <arvf />
-        </div>
-        <div class="mt-20 ml-10 mb-20" v-show="phageStore.phagetransprotein.length !== 0">
-            <transprotein />
         </div>
     </div>
-
-    <el-dialog v-model="proteinVisible" title="Protein Detail" width="90%">
-        <proteindetail v-model:proteinInfo="proteinInfo" v-model:dataset="datasets" />
-    </el-dialog>
     <el-dialog
         v-model="downloadphagedialogVisible"
         title="Select download data"
@@ -211,36 +147,19 @@
 import { CloudDownloadOutline as di, AddCircleOutline as selectIcon } from '@vicons/ionicons5'
 //
 import axios from 'axios'
-// import { NButton, NTag } from 'naive-ui'
-// import _ from 'lodash'
 import { reactive, ref } from 'vue'
-// import { datasetList } from '@/utils/phage'
-import proteindetail from '../protein/detail.vue'
-// import annotation from '../../visualize/components/annotation.vue'
-// import details_linechart from '../../visualize/components/details_linechart.vue'
-// import circo from '../../visualize/components/circo.vue'
-// import terminator from '../phage/terminator.vue'
-// import anti from '../phage/anti.vue'
-// import crispr from '../phage/crispr.vue'
-// import trna from '../phage/trna.vue'
-// import transprotein from '../phage/transprotein.vue'
-// import arvf from '../phage/arvf.vue'
+import * as echarts from 'echarts'
+import _ from 'lodash'
 import { usePhageStore } from '@/store/phage'
-import { useCrustDBStore } from '@/store/crustdb'
-// import { useVisualizeStore } from '@/store/visualize'
-// const visualizeStore = useVisualizeStore()
+// import { useCrustDBStore } from '@/store/crustdb'
 
 const phageStore = usePhageStore()
-const crustdbStore = useCrustDBStore()
+// const crustdbStore = useCrustDBStore()
 const loaddata = ref(false)
-// const annoshow = ref(true)
-// const showswicth = () => {
-//     annoshow.value = !annoshow.value
-// }
 const route = useRoute()
-// const phageid = computed(() => route.query?.phageid as number)
 const phageid = computed(() => route.query?.crustdb_main_id as number)
 const repeatuid = computed(() => route.query?.details_uid as string)
+let mylineEcharts
 
 const phagedata = ref({
     ST_platform: '',
@@ -267,10 +186,18 @@ const detailsdata = ref({
     distance_list: [],
 })
 
-const proteinVisible = ref(false)
-// const proteindata = ref([] as any[])
-const proteinInfo = ref()
-const datasets = ref('')
+const echartlineDom = ref<HTMLElement | null>(null)
+// const linechartvalue = ref('Convergence of Conformation with Cytocraft')
+// const linechartoptions = [
+//     {
+//         label: 'ST Platform Distribution',
+//         value: 'ST Platform Distribution',
+//     },
+//     {
+//         label: 'ST Platform Distribution',
+//         value: 'ST Platform Distribution',
+//     },
+// ]
 
 const downloadphagedialogVisible = ref(false)
 
@@ -306,9 +233,8 @@ const selectRepeatRequest = async () => {
         })
     } else {
         const data_uid = selectRepeatCheckList.value[0].split(' ')[2]
-        console.log(data_uid) // Stage44.CP_1XOH
+        // console.log(data_uid) // Stage44.CP_1XOH
         selectRepeatCheckList.value.length = 0 // clear the checkList
-        // window.open(`/api/database/crustdb_main/detail?id=${phageid.value}`, '_blank')
         const response2 = await axios.get(`/details`, {
             baseURL: '/api',
             timeout: 10000,
@@ -316,14 +242,57 @@ const selectRepeatRequest = async () => {
                 details_uid: data_uid, // details.repeat_data_uid
             },
         })
-        // console.log('=============================== response2')
-        // console.log(response2)
         detailsdata.value = response2.data
-        crustdbStore.detailsDistanceList = detailsdata.value.distance_list
         selectRepeatDialogVisible.value = false
     }
 }
 
+const chartOption = () => {
+    mylineEcharts.setOption({
+        title: {
+            text: `Convergence of Conformation with Cytocraft\n${detailsdata.value.repeat_data_uid}`,
+            left: 'center',
+        },
+        tooltip: {},
+        toolbox: {
+            itemSize: 20,
+            iconStyle: {
+                borderColor: '#34498e',
+            },
+            feature: {
+                dataView: { readOnly: true },
+                // magicType: { type: ['line', 'bar'] },
+                // restore: {},
+                saveAsImage: {},
+            },
+        },
+        xAxis: {
+            name: 'Iteration',
+            nameLocation: 'middle',
+            nameGap: 30,
+            scale: true,
+            data: _.range(detailsdata.value.distance_list.length),
+            axisLabel: {
+                rotate: 0,
+                fontSize: 13,
+            },
+        },
+        yAxis: {
+            name: 'cvRMSD',
+            nameLocation: 'middle',
+            nameGap: 30,
+            scale: true,
+            type: 'value',
+        },
+        series: [
+            {
+                name: 'CrustDB',
+                type: 'line',
+                data: detailsdata.value.distance_list,
+            },
+        ],
+    })
+}
 const updateRepeatList = () => {
     repeat_data_uid_list.value.length = 0
     for (let i = 0; i < phagedata.value.repeat_data_uid_list.length; i += 1) {
@@ -332,17 +301,15 @@ const updateRepeatList = () => {
         )
     }
 }
-
+const selectRepeat = () => {
+    updateRepeatList()
+    selectRepeatDialogVisible.value = true
+}
 const download = () => {
     updateRepeatList()
     downloadtype.value = 'single'
     downloadphagedialogVisible.value = true
     checkedRowKeysRef.value = [phageid.value]
-}
-
-const selectRepeat = () => {
-    updateRepeatList()
-    selectRepeatDialogVisible.value = true
 }
 
 onBeforeMount(async () => {
@@ -364,7 +331,6 @@ onBeforeMount(async () => {
     // From table details
     let response2 = null
     if (repeatuid.value === '') {
-        // alert(phagedata.value.uniq_data_uid)
         response2 = await axios.get(`/details`, {
             baseURL: '/api',
             timeout: 10000,
@@ -383,53 +349,18 @@ onBeforeMount(async () => {
     }
 
     detailsdata.value = response2.data // show the 1st repeat, by default
-    crustdbStore.detailsDistanceList = detailsdata.value.distance_list
-    // alert(detailsdata.value)
-
-    // phageStore.phageaccid = data.Acession_ID
-    // const response2 = await axios.get(`/proteins`, {
-    //     baseURL: '/api',
-    //     timeout: 10000,
-    //     params: {
-    //         phageid: phageid.value,
-    //     },
-    // })
-    // const prodata = response2.data
-    // proteindata.value = prodata
-    // phageStore.phageprotein = prodata
+    // crustdbStore.detailsDistanceList = detailsdata.value.distance_list
     loaddata.value = false
-
-    // const response3 = await axios.get(`phage/fasta/`, {
-    //     baseURL: '/api',
-    //     timeout: 10000,
-    //     params: {
-    //         phageid: phageid.value,
-    //     },
-    // })
-
-    // phageStore.phagefastadata = response3.data
-    // phageStore.phagedataloaded = true
+})
+onMounted(async () => {
+    mylineEcharts = echarts.init(echartlineDom.value as HTMLElement)
+    chartOption()
 })
 
-// const phagedetail = computed(() => phagedata.value)
+watch(detailsdata, () => {
+    chartOption()
+})
 
-// const proteinList = computed(() => proteindata.value)
-// const genes = computed(() => {
-//     return proteinList.value.length
-// })
-
-// const formatLengthNumber = num => {
-//     return num
-// if (num >= 1000000) {
-//     return `${(num / 1000000).toFixed(0)}, ${((num / 1000).toFixed(0) % 1000)
-//         .toString()
-//         .padStart(3, '0')}, ${(num % 1000).toString().padStart(3, '0')}`
-// }
-// if (num >= 1000) {
-//     return `${(num / 1000).toFixed(0)}, ${(num % 1000).toString().padStart(3, '0')}`
-// }
-// return num.toString()
-// }
 const pagination = reactive({
     page: 1,
     pageSize: 30,
@@ -443,237 +374,4 @@ const pagination = reactive({
         pagination.page = 1
     },
 })
-// type RowData = {
-//     id: number
-//     Phage_Acession_ID: string
-//     protein_id: string
-//     annotate_Source: string
-//     protein_product: string
-//     protein_sort_class: string
-//     start_point: string
-//     end_point: string
-//     strand: string
-// }
-// const rowKey = (row: RowData) => {
-//     return row.id
-// }
-// const createColumns = (): DataTableColumns<RowData> => [
-//     {
-//         type: 'selection',
-//     },
-//     {
-//         title: 'Protein ID',
-//         key: 'Protein_id',
-//         align: 'center',
-//         width: '150',
-//     },
-//     {
-//         title: 'Source',
-//         key: 'Source',
-//         align: 'center',
-//         width: '120px',
-//     },
-//     {
-//         title: 'Function classification',
-//         key: 'Protein_function_classification',
-//         align: 'center',
-//         width: '250px',
-//         filterOptions: [
-//             {
-//                 label: 'hypothetical',
-//                 value: 'hypothetical',
-//             },
-//             {
-//                 label: 'infection',
-//                 value: 'infection',
-//             },
-//             {
-//                 label: 'assembly',
-//                 value: 'assembly',
-//             },
-//             {
-//                 label: 'unsorted',
-//                 value: 'unsorted',
-//             },
-//             {
-//                 label: 'replication',
-//                 value: 'replication',
-//             },
-//             {
-//                 label: 'packaging',
-//                 value: 'packaging',
-//             },
-//             {
-//                 label: 'lysis',
-//                 value: 'lysis',
-//             },
-//             {
-//                 label: 'regulation',
-//                 value: 'regulation',
-//             },
-//             {
-//                 label: 'immune',
-//                 value: 'immune',
-//             },
-//             {
-//                 label: 'integration',
-//                 value: 'integration',
-//             },
-//             {
-//                 label: 'tRNA',
-//                 value: 'tRNA',
-//             },
-//             {
-//                 label: 'mix',
-//                 value: 'mix',
-//             },
-//         ],
-//         filter(value: any, row: any) {
-//             return row.Protein_function_classification.includes(value)
-//         },
-//         render(row: any) {
-//             const tagnode = []
-//             const classlist = row.Protein_function_classification.split(';').slice(
-//                 0,
-//                 -1
-//             ) as string[]
-//             classlist.forEach((item, index) => {
-//                 tagnode.push(
-//                     h(
-//                         NTag,
-//                         {
-//                             size: 'small',
-//                             type: 'info',
-//                         },
-//                         {
-//                             default: () => {
-//                                 return classlist[index]
-//                             },
-//                         }
-//                     )
-//                 )
-//             })
-//             return h(
-//                 'div',
-//                 { style: { width: '250px', display: 'flex', justifyContent: 'space-around' } },
-//                 tagnode
-//             )
-//         },
-//     },
-//     {
-//         title: 'Protein Product',
-//         key: 'Protein_product',
-//         align: 'center',
-//         width: '250px',
-//         ellipsis: {
-//             tooltip: true,
-//         },
-//     },
-//     {
-//         title: 'Start Point',
-//         key: 'Start_location',
-//         align: 'center',
-//         width: '120px',
-//         sorter: 'default',
-//     },
-//     {
-//         title: 'End Point',
-//         key: 'Stop_location',
-//         align: 'center',
-//         width: '120px',
-//         sorter: 'default',
-//     },
-//     {
-//         title: 'Strand',
-//         key: 'Strand',
-//         width: '90px',
-//         align: 'center',
-//     },
-//     {
-//         title: 'Action',
-//         key: 'actions',
-//         width: 100,
-//         align: 'center',
-//         render(row) {
-//             return h(
-//                 'div',
-//                 {
-//                     style: {
-//                         display: 'flex',
-//                         justifyContent: 'center',
-//                     },
-//                 },
-//                 [
-//                     h(
-//                         NButton,
-//                         {
-//                             strong: true,
-//                             tertiary: true,
-//                             size: 'small',
-//                             type: 'info',
-//                             onClick: () => {
-//                                 proteinInfo.value = row
-//                                 proteinVisible.value = true
-//                             },
-//                         },
-//                         { default: () => 'Detail' }
-//                     ),
-//                 ]
-//             )
-//         },
-//     },
-// ]
-// const columns = createColumns()
-// const complete = (comp: any) => {
-//     if (comp === 'Medium-quality') {
-//         return 'info'
-//     }
-//     if (comp === 'High-quality') {
-//         return 'success'
-//     }
-//     if (comp === 'Low-quality') {
-//         return 'warning'
-//     }
-//     if (comp === 'Complete') {
-//         return 'success'
-//     }
-//     return 'warning'
-// }
-// const lifestyleColor = (style: any) => {
-//     if (style === 'virulent') {
-//         return 'error'
-//     }
-//     return 'info'
-// }
-// const downloadtsv = () => {
-//     console.log(proteinList.value)
-//     if (proteinList.value.length !== 0) {
-//         const headers = _.keys(proteinList.value[0])
-//         const formattedData = [headers]
-//         proteinList.value.forEach(item => {
-//             const row = _.values(item)
-//             formattedData.push(row)
-//         })
-//         const tsvContent = formattedData.map(row => row.join('\t')).join('\n')
-//         const blob = new Blob([tsvContent], { type: 'text/tab-separated-values' })
-//         const url = URL.createObjectURL(blob)
-//         const link = document.createElement('a')
-//         link.href = url
-//         link.download = 'gene_annotation.tsv'
-//         link.style.display = 'none'
-//         document.body.appendChild(link)
-//         link.click()
-//         URL.revokeObjectURL(url)
-//         document.body.removeChild(link)
-//     }
-// }
-// const gohost = () => {
-//     router.push({
-//         path: `/database/host/list`,
-//         query: {
-//             rank: 'host',
-//             node: phagedetail.value.host,
-//         },
-//     })
-// }
 </script>
